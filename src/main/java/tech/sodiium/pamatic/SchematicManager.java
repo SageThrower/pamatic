b454558f3fa05448e1e089faf8a62022b508afce
Samuel Albani <sodium@sodiium.tech>
2020-08-13 14:29:34 +0200

    Added a fill blocks in region function

--- src/main/java/tech/sodiium/pamatic/SchematicManager.java
+++ src/main/java/tech/sodiium/pamatic/SchematicManager.java
@@ -2,11 +2,17 @@ package tech.sodiium.pamatic;
 
 import com.boydti.fawe.FaweAPI;
 import com.boydti.fawe.object.RelightMode;
+import com.boydti.fawe.util.EditSessionBuilder;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.function.pattern.Pattern;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.world.block.BlockTypes;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 
 import java.io.File;
@@ -22,6 +28,12 @@ class Coordinate extends BlockVector3 {
         this.x = (int)location.getX();
         this.y = (int)location.getY();
         this.z = (int)location.getZ();
+    }
+
+    public Coordinate(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public int getX() {
@@ -71,5 +83,24 @@ public class SchematicManager {
 
     public static void pasteSchematic(File schematicFile, int x, int y, int z, World world) throws IOException {
         pasteSchematic(fileToClipboard(schematicFile), x, y, z, world);
+    }
+
+    public static void fillBlocks(Region region, com.sk89q.worldedit.world.World world, Pattern pattern) {
+        EditSession editSession = new EditSessionBuilder(world).fastmode(true).build();
+        editSession.setBlocks(region, pattern);
+        editSession.flushQueue();
+        FaweAPI.fixLighting(world, region, null, RelightMode.ALL);
+    }
+
+    public static void fillBlocks(int x1, int y1, int z1, int x2, int y2, int z2, World world, Material block) {
+        fillBlocks(new CuboidRegion(new Coordinate(x1, y1, z1), new Coordinate(x2, y2, z2)),
+                FaweAPI.getWorld(world.getName()),
+                BlockTypes.parse(block.name()));
+    }
+
+    public static void fillBlocks(Location location1, Location location2, Material block) {
+        fillBlocks(new CuboidRegion(new Coordinate(location1), new Coordinate(location2)),
+                FaweAPI.getWorld(location1.getWorld().getName()),
+                BlockTypes.parse(block.name()));
     }
 }
